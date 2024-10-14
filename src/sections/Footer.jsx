dd0467396e056cdca6a8ab4451903d3b65a8db08
Daniel Oladele <danieloladele7@gmail.com>
2024-10-14 13:49:12 +0200

    site updated

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer-container">
+    <footer className="footer-container pb-0">
       <div className="py-6">
       <p className="text-sm max-w-[85vw] text-center text-contactBorder font-semibold font-afacad">Copyright © 2024, All rights reserved | Terms and Conditions | Privacy Policy | Created by WebCronX</p>
       </div>
